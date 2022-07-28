919c68b417b477e35eec89e6e78f59fd43b5e730
WILLIANsAccount <willianrodrigues310@gmail.com>
2022-07-28 17:47:58 -0300

    Estructura de carpetas

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const publicPath = path.resolve(__dirname, "./public");
+app.use( express.static(publicPath) );
+
+app.listen(3000, () => {
+    console.log("Servidor corriendo en el puerto 3000");
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/home.html"));
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "/register"));
+});
