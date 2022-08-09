cdbe9af0eb0113de2eb8183c475e68fc311e56e3
name <juancruzberrios73@gmail.com>
2022-08-09 18:09:53 -0300

    rutas arregladas

--- source/app.js
+++ source/app.js
@@ -3,10 +3,10 @@ const path = require("path");
 
 const app = express();
 
-const publicPath = path.resolve(__dirname, "./public");
-app.use( express.static(publicPath) );
+const publicPath = path.resolve(__dirname, "../public");
+app.use(express.static(publicPath) );
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Servidor corriendo en el puerto 3000");
 });
 
