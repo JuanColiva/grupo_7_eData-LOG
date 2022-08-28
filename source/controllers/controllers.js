96081de1c3c9775b7d414eef2370550bf430eba3
JuanColiva <juanchioliva1@gmail.com>
2022-08-28 18:44:07 -0300

    delete, edit and create

--- source/controllers/controllers.js
+++ source/controllers/controllers.js
@@ -1,5 +1,7 @@
-const e = require("express");
-const {join} = require("path");
+const app = require("express");
+const {unlinkSync} = require ("fs");
+const {resolve} = require ("path");
+const {join} = require("path")
 const {all, one, generate, write} = require("../models/products.models");
 const controller = {
     home: (req, res) => {
@@ -50,10 +52,14 @@ const controller = {
 
     },
     create: (req, res) =>{
+        
         let file = join(__dirname, "../views/users/create.ejs");
         res.render(file);
       },
       save: (req, res) =>{
+        if (req.files && req.files.length > 0){
+            req.body.imagen = req.files && req.files.length> 0 ? req.files[0].filename : "default.png"
+           }
         let nuevo = generate (req.body);
         let todos = all();
         todos.push(nuevo)
@@ -73,7 +79,7 @@ const controller = {
                 elemento.name = req.body.name;
                 elemento.plan = req.body.plan;
                 elemento.descripcion = req.body.descripcion;
-                elemento.image = req.files && req.files.length > 0 ? req.files[0].filename : elemento.image
+                elemento.imagen = req.files && req.files.length > 0 ? req.files[0].filename : elemento.imagen
             }
             return elemento
         
@@ -83,6 +89,7 @@ const controller = {
         return res.redirect("/productos")
       },
       remove: (req,res)=>{
+        let product = one(req.body.id)
         let todos = all();
         let noEliminados = todos.filter(elemento => elemento.id != req.body.id)
         write(noEliminados)
