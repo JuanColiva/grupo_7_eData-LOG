ccc9f189ce12b31ad3c619ef17740b595a010eb7
juanB <juancruzberrios73@gmail.com>
2022-11-12 16:09:03 -0300

    backup

--- website/source/app.js
+++ website/source/app.js
@@ -6,6 +6,7 @@ const methodOverride = require("method-override")
 const session = require("express-session")
 const cookie = require("cookie-parser")
 const {join,} = require("path");
+const cors = require('cors') 
 
 app.set("views", join(__dirname, "/views"))
 
@@ -25,6 +26,7 @@ app.use(session({
 }))
 app.use(cookie()) 
 app.use(require("./middlewares/users"))
+app.use(cors())
 app.use(require("./routes/routes.js"));
 app.use('/users', require('./routes/users.routes'))
 app.use("/api", require("./routes/api/users"))
--- website/source/modules/server.js
+++ website/source/modules/server.js
@@ -1,4 +1,4 @@
-const port = process.env.PORT || 3000
-const start =  () => console.log("Servidor corriendo en el puerto 3000");
+const port = process.env.PORT || 3001
+const start =  () => console.log("Servidor corriendo en el puerto 3001");
 
 module.exports = {port, start}
